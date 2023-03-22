d1f02e31c80228bdbdf575964149d4eac402edf6
Avni Patel <pavni201@gmail.com>
2023-03-22 13:14:23 -0400

    modified:   frontend/login/common.js 	modified:   frontend/login/form.js

--- frontend/login/common.js
+++ frontend/login/common.js
@@ -1,3 +1,16 @@
-const showFormError = (err) => {
-    let errorEle = documnet.querySelector ('.error');
+const sendData = (path, data) => {
+    console.log(data);
+    fetch(path, {
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify(data)
+    })
+    .then(res => res.text())
+    .then(data => processData(data));
+
 }
+
+const processData = (data) => {
+    console.log(data);
+}
+
--- frontend/login/form.js
+++ frontend/login/form.js
@@ -3,12 +3,34 @@ let formBtn = document.querySelector('.submit-btn');
 formBtn.addEventListener('click', () => {
     let fullname = document.querySelector('#name');
     let email = document.querySelector('#email');
-    let password = document.querySelector('#password');
-    let tac = document.querySelector('#tc')
+    let password = document.querySelector('#password') || null;
+    let tac = document.querySelector('#tc') || null;
 
-    if(fullname.value.length < 1){
+if(fullname != null){ //signup page
+    if(fullname.value.length < 1){ 
         alert('Please enter your name');
+    } else if(!email.value.length){
+        alert('Enter your email');
+    } else if(email.value.length<8){
+        alert('Your password must be at least 8 characters long');
+    } else if(!tac.checked){
+        alert('You muust agree to our terms and conditions');
+    } else{
+        sendData('/users',{
+            name: fullname.value,
+            email: email.value,
+            password: password.value,
+            tac: tac.checked
+        })
     }
+} else{
+    if(!email.value.length || !password.value.length){
+        alert('Fill all the inputs')
+        sendData('/users',{
+            email: email.value,
+            password: password.value,
+        })
+    }
+}
 
 })
-
