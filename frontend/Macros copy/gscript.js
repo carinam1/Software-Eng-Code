1b7227055637dc90379fea9fbd052f57223cc405
Juliorodrigo23 <23juliorod@gmail.com>
2023-04-26 02:41:30 -0400

    i dont even know what i think

--- frontend/Macros copy/gscript.js
+++ frontend/Macros copy/gscript.js
@@ -11,7 +11,6 @@ function submitText() {
     fetch('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=vJbx4iLpZCsafpmVwdAFzQciR9a0LKp782rPLmYn&query='+text)
         .then(response => response.json())
         .then(data => {
-            // Display the results on the page
             let foods = data.foods;
 
             let select = document.getElementById('data-select');
@@ -25,23 +24,18 @@ function submitText() {
             let container = document.getElementById('data-container');
 
             select.onchange = function() {
-                // Get the selected option's value
                 let selectedId = this.value;
                 fetch("https://api.nal.usda.gov/fdc/v1/food/"+selectedId+"?api_key=vJbx4iLpZCsafpmVwdAFzQciR9a0LKp782rPLmYn")
                     .then(response => response.json())
                     .then(data => {
-                        console.log(data);
                         food = data;
-                        //let portions = data.foodPortions;
                         let nutrients = data.foodNutrients;
-                        
-                        // Get nutrient values
+
                         calories = getNutrient(nutrients, 'Energy');
                         protein = getNutrient(nutrients, 'Protein');
                         fat = getNutrient(nutrients, 'Total lipid (fat)');
                         carbohydrate = getNutrient(nutrients, 'Carbohydrate, by difference');
-                
-                        // Update container's innerHTML
+
                         container.innerHTML = `
                             <h2>${food.description}</h2>
                             <p>Calories: ${calories}</p>
@@ -49,12 +43,22 @@ function submitText() {
                             <p>Fat: ${fat}</p>
                             <p>Carbohydrates: ${carbohydrate}</p>
                         `;
-                    })
-                ;
+
+                        // Send the food data to the Cloud Function
+                        sendFoodToDatabase(
+                            food.description,
+                            parseFloat(calories.split(' ')[0]),
+                            parseFloat(protein.split(' ')[0]),
+                            parseFloat(fat.split(' ')[0]),
+                            parseFloat(carbohydrate.split(' ')[0])
+                        );
+                    });
             };
     })
     .catch(error => console.error(error));
 }
+
+
 
 function getNutrient(nutrientData, nutrientName) {
     let temp = nutrientData.filter(a => a.nutrient.name === nutrientName)[0];
@@ -66,12 +70,12 @@ function getNutrient(nutrientData, nutrientName) {
     }
 }
 
-function addToLogTable(foodName, calories, protein, fat, carbs) {
-    sendFoodToDatabase(foodName, calories, protein, fat, carbs);
-    
+function addToLogTable(foodId, foodName, calories, protein, fat, carbs) {
     const tableBody = document.getElementById("log-table-body");
     const row = tableBody.insertRow();
     
+    row.dataset.foodId = foodId; // Add the foodId as a data attribute
+
     row.insertCell().innerHTML = foodName;
     row.insertCell().innerHTML = calories;
     row.insertCell().innerHTML = protein;
@@ -84,6 +88,9 @@ function addToLogTable(foodName, calories, protein, fat, carbs) {
         removeFromLog(row, calories, protein, fat, carbs);
     };
     const removeCell = row.insertCell();
+    removeButton.onclick = async function () {
+        await removeFromLog(row, foodId, calories, protein, fat, carbs);
+      };      
     removeCell.className = "remove-cell";
     removeCell.appendChild(removeButton);
 }
@@ -112,16 +119,43 @@ async function sendFoodToDatabase(foodName, calories, protein, fat, carbs) {
     }
   }
 
-function removeFromLog(row, calories, protein, fat, carbs) {
+async function removeFoodFromDatabase(foodId) {
+    const requestBody = {
+      food_id: foodId,
+    };
+  
+    const response = await fetch('https://us-central1-nuyu-381420.cloudfunctions.net/removeFood', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    });
+  
+    if (response.ok) {
+      console.log('Food removed successfully');
+    } else {
+      console.error('Error removing food from the database');
+    }
+  }
+  
+
+  async function removeFromLog(row, foodId, calories, protein, fat, carbs) {
     dailyMacros.calories -= parseFloat(calories.split(" ")[0]);
     dailyMacros.protein -= parseFloat(protein.split(" ")[0]);
     dailyMacros.fat -= parseFloat(fat.split(" ")[0]);
     dailyMacros.carbohydrates -= parseFloat(carbs.split(" ")[0]);
-
+  
     document.getElementById("daily-calories").innerHTML = `Total Daily Calories: ${dailyMacros.calories.toFixed(2)}`;
     document.getElementById("daily-protein").innerHTML = `Total Daily Protein: ${dailyMacros.protein.toFixed(2)} g`;
     document.getElementById("daily-fat").innerHTML = `Total Daily Fat: ${dailyMacros.fat.toFixed(2)} g`;
     document.getElementById("daily-carbohydrates").innerHTML = `Total Daily Carbohydrates: ${dailyMacros.carbohydrates.toFixed(2)} g`;
-
-    row.parentNode.removeChild(row);
-}
+  
+    try {
+      await removeFoodFromDatabase(foodId);
+      row.parentNode.removeChild(row);
+    } catch (error) {
+      console.error("Error removing food from the database");
+    }
+  }
+  
